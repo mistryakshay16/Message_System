731efb179238ccd3f5833d1ea936cd8d02a6fa41
Rishabh0507 <pandey.rajat05@gmail.com>
2019-01-30 16:41:31 +0530

    ragister and connection modified

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 /* Database connection start */
 $servername = "localhost";
 $username = "root";
-$password = "123456";
+$password = "";
 $dbname = "msg_system";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
